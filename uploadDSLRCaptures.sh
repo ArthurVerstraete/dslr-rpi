9ad2291d62b16ff50e67948a5cac022221925610
Arthur Verstraete <verstraetearthur@gmail.com>
2019-12-11 23:16:32 +0100

    finished script

--- uploadDSLRCaptures.sh
+++ uploadDSLRCaptures.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-
+# remove old images in img folder
 rm img/DSC*
+
+# capture images and download on the pi + keeping the image on DSLR
 gphoto2 --capture-image-and-download --keep
+
+# move image to img folder
 mv DSC* img/
 
-
-# just for testing commented this line:
-# ./dropbox_uploader.sh
+# use dropbox uploader script to upload script to dropbox
+# at first run, this will trigger a configuration for accessing your dropbox
+./dropbox_uploader.sh upload img/DSC* /
