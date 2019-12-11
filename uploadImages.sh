aeead32998e2ccc1e0a847c12a6c0393c7f241b9
Arthur Verstraete <verstraetearthur@gmail.com>
2019-12-11 23:44:05 +0100

    made script for just uploading the images, but it's not working yet

---
+++ uploadImages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# remove old images in img folder
+rm img/DSC*
+
+# capture images and download on the pi + keeping the image on DSLR
+gphoto2 --get-all-files --new
+
+# move image to img folder
+mv DSC* img/
+
+# use dropbox uploader script to upload script to dropbox
+# at first run, this will trigger a configuration for accessing your dropbox
+./dropbox_uploader.sh upload img/DSC* /
