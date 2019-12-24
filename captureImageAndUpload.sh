eac8cdb272d06dd23d4e764a00adc5bebf13f4a8
Arthur Verstraete <verstraetearthur@gmail.com>
2019-12-24 11:28:25 +0100

    update script

--- captureImageAndUpload.sh
+++ captureImageAndUpload.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # remove old images in img folder
-rm img/DSC*
+rm img/*.JPG
 
 # capture images and download on the pi + keeping the image on DSLR
 gphoto2 --capture-image-and-download --keep
 
 # move image to img folder
-mv DSC* img/
+mv *.JPG img/
 
 # use dropbox uploader script to upload script to dropbox
 # at first run, this will trigger a configuration for accessing your dropbox
-./dropbox_uploader.sh upload img/DSC* /
+./dropbox_uploader.sh upload img/*.JPG /
